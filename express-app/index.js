6f887e87c47db017e950bcb81d2e546868d7214a
Shane Chintatkun <schintatkun@gmail.com>
2019-01-27 14:59:35 -0800

    add a static route for file serving

--- express-app/index.js
+++ express-app/index.js
@@ -1,9 +1,39 @@
 import express from 'express';
 import data from './data/data.json';
 
-
 const app = express();
 const PORT = 3000;
+
+//How to load static file in server
+//Express.static method that is exist in express API
+//.use is a method that allow to use specific middleware to a path
+//After express.static() that require to specify which folder need to be used
+app.use(express.static('public'));
+
+//this is for images folder on path /images
+app.use('/images', express.static('images'));
+
+
+
+// call method that you want to run
+// get method take an actual path, arguments (req, res)
+// then run handler
+app.get('/', (req, res) => 
+    //get data
+    res.json(data)
+
+    // res.send(`a get request with / route on port ${PORT}`)
+);
+app.post('/newItem', (req, res) => 
+    res.send(`a post request with /newItem route on port ${PORT}`)
+);
+// put method to update data
+app.put('/item', (req, res) => 
+    res.send(`a put request with /item route on port ${PORT}`)
+);
+app.delete('/item', (req, res) => 
+    res.send(`a delete request with /item route on port ${PORT}`)
+);
 
 app.listen(PORT, () => {
     console.log(`Your server is running on port ${PORT}`)
